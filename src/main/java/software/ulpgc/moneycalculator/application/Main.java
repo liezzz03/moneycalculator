f714d3cb6046cd016cf104f951aa8aa9f880b98f
Paula <paulalijunuezhernandez@gmail.com>
2026-01-02 15:48:15 +0000

    Updated Main to visualize the mainFrame

--- src/main/java/software/ulpgc/moneycalculator/application/Main.java
+++ src/main/java/software/ulpgc/moneycalculator/application/Main.java
@@ -1,16 +1,36 @@
 package software.ulpgc.moneycalculator.application;
 
+import software.ulpgc.moneycalculator.control.Command;
+import software.ulpgc.moneycalculator.control.ExchangeMoneyCommand;
+import software.ulpgc.moneycalculator.io.currency.*;
+import software.ulpgc.moneycalculator.io.exchangeRate.ExchangeRateLoader;
+import software.ulpgc.moneycalculator.io.exchangeRate.FixerExchangeRateDeserializer;
+import software.ulpgc.moneycalculator.io.exchangeRate.FixerExchangeRateLoader;
+import software.ulpgc.moneycalculator.io.exchangeRate.FixerExchangeRateReader;
 import software.ulpgc.moneycalculator.model.Currency;
-import software.ulpgc.moneycalculator.model.ExchangeRate;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        List<Currency> currencies = new WebService.CurrencyLoader().loadAll();
-        ExchangeRate exchangeRate = new WebService.ExchangeRateLoader().load(currencies.get(0), currencies.get(1));
-        System.out.println(exchangeRate);
+    public static void main(String[] args) {
+        CurrencyLoader loader = new FixerCurrencyLoader(
+                new FixerCurrencyReader(),
+                new FixerCurrencyDeserializer(),
+                new FixerCurrencyAdapter()
+        );
+        List<Currency> currencies = loader.load();
+        MainFrame mainFrame = new MainFrame(currencies);
+        ExchangeRateLoader exchangeRateLoader = new FixerExchangeRateLoader(
+                new FixerExchangeRateReader(),
+                new FixerExchangeRateDeserializer()
+        );
+        Command exchangeCommand = new ExchangeMoneyCommand(
+                mainFrame.getMoneyDialog(),
+                mainFrame.getCurrencyDialog(),
+                exchangeRateLoader,
+                mainFrame.getMoneyDisplay()
+        );
+        mainFrame.put("Calculate", exchangeCommand);
+        mainFrame.setVisible(true);
     }
 }
