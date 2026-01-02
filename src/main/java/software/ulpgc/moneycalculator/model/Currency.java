9c3d5f98602f42afcf1979d533a528f831aa5bcc
Paula <paulalijunuezhernandez@gmail.com>
2026-01-02 15:42:49 +0000

    Minor changes for Currency & Money

--- src/main/java/software/ulpgc/moneycalculator/model/Currency.java
+++ src/main/java/software/ulpgc/moneycalculator/model/Currency.java
@@ -1,4 +1,8 @@
 package software.ulpgc.moneycalculator.model;
 
 public record Currency(String code, String country) {
+    @Override
+    public String toString() {
+        return code + "-" + country;
+    }
 }
