f04442b559033f7ef90a8c5668c3b774c266980c
Paula <paulalijunuezhernandez@gmail.com>
2026-01-02 15:41:31 +0000

    Created class FixerAPI

---
+++ src/main/java/software/ulpgc/moneycalculator/io/FixerAPI.java
@@ -0,0 +1,7 @@
+package software.ulpgc.moneycalculator.io;
+
+public class FixerAPI {
+    public static final String key = "5bb419a809e62ec0554bd54bbd7b0f03";
+    public static final String getSymbol = "http://api.exchangeratesapi.io/v1/symbols?access_key=";
+    public static final String getExchangeRate = "http://api.exchangeratesapi.io/v1/latest?access_key=";
+}
